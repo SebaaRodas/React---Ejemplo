fdb534dd487b856d034d17df4f2ffae676245caf
Seba <sebastianrodas11@gmail.com>
2021-07-01 12:48:44 -0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Hola Mundo
+          Ejemplo de error
         </h1>
         <a
           className="App-link"
