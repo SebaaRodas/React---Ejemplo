2f8b04e04ee3930edfe2c24e44f0b44c18a5781b
Seba <sebastianrodas11@gmail.com>
2021-07-01 04:02:38 -0300

    prueba

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hola Mundo/i);
+  const linkElement = screen.getByText(/Ejemplo/i);
   expect(linkElement).toBeInTheDocument();
 });
